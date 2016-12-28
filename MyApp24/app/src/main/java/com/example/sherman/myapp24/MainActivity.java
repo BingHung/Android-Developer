15024f24d545f93c0fddc734b6ee0757831c7201
BH <cooky11048.me00@g2.nctu.edu.tw>
2016-12-28 11:04:41 +0800

    Android Launch

--- MyApp24/app/src/main/java/com/example/sherman/myapp24/MainActivity.java
+++ MyApp24/app/src/main/java/com/example/sherman/myapp24/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.sherman.myapp24;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View; //handling event ...
+import android.widget.Button;
+import android.widget.TextView;
+
+// Event handling (Divided into Two Parts) -> (Event Listener , Call Back Method)
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,38 +14,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //detect event  -> reference to button (ID)
+        Button myButton = (Button) findViewById(R.id.mybtn); //(Type Casting)
+
+        // Add Action listener
+        myButton.setOnClickListener (
+
+            // create interface
+                new Button.OnClickListener() {
+
+                    // add call back method in this interface
+                    public void onClick(View v) {
+                        TextView myText = (TextView) findViewById(R.id.myText);
+                        myText.setText("Button Clicked");
+                    }
+                }
+        );
+
+        myButton.setOnLongClickListener(
+
+                new Button.OnLongClickListener() {
+                    public boolean onLongClick(View v)  {
+                        TextView myText = (TextView) findViewById(R.id.myText);
+                        myText.setText("Long Button Press");
+                        return true;
+                    }
+                }
+        );
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
+
